01a9d02b0c4827bf272caa5737952793a04425d6
Bonuspunkt <bonuspunkt+github@gmail.com>
2014-12-03 04:11:28 +0100

    added download & preview to map list

--- views/maps.jsx
+++ views/maps.jsx
@@ -2,18 +2,27 @@ var React = require('react');
 var DefaultLayout = require('./layouts/default');
 var mapTypes = require('../const').mapTypes;
 
+var pad = 2;
+var floatWidth = 180;
+
 var Maps = React.createClass({
   render: function() {
     var maps = this.props.maps || [];
 
     var mapEls = maps.map(function(map) {
       return (
-        <div key={map.id}>
+        <div key={map.id} className="mapEntry" style={{position: 'relative', padding: pad, paddingRight: floatWidth }}>
           { map.updated.toJSON() }
           <span> - </span>
           <a href={ '/u/' + map.authorid }>{ map.authorname }</a>
           <span> / </span>
           <a href={ '/m/' + map.id }>{map.name || map.filename }</a>
+          
+          <div style={{ position: 'absolute', right: pad, top: pad, textAlign: 'right', width: floatWidth }}>
+            (<a href={ "/webView/#/dl/" + map.filename + '.map' }>preview</a>)
+            {' '} (<a href={ '/dl/' + map.filename + '.map' }>download</a>)
+          </div>
+
         </div>
       );
     });
